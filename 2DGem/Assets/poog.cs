8f3e092ad0b4a0c8169da20f50eec2000cb852a0
dinobotterd <brycelepat@gmail.com>
2021-04-01 16:35:45 +0200

    Menu And camera

---
+++ 2DGem/Assets/poog.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class poog : MonoBehaviour
+{
+
+public Transform player;
+public Vector3 offset;
+
+
+    
+     void Update() 
+  {
+      transform.position = new Vector3 (player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+  }
+}
+  
+  
