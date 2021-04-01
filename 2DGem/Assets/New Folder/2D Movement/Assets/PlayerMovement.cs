74d744f833779f7c9d0848e32c1241d43bd1aefc
MPC-sys <81619477+MPC-sys@users.noreply.github.com>
2021-04-01 08:51:09 +0200

    code changes

--- 2DGem/Assets/New Folder/2D Movement/Assets/PlayerMovement.cs
+++ 2DGem/Assets/New Folder/2D Movement/Assets/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public CharacterController2D controller;
-
+	public Animator animator;
 	public float runSpeed = 40f;
 
 	float horizontalMove = 0f;
@@ -16,6 +16,8 @@ public class PlayerMovement : MonoBehaviour {
 	void Update () {
 
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		animator.SetFloat("Speed")
 
 		if (Input.GetButtonDown("Jump"))
 		{
